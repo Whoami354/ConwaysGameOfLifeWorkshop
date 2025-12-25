ac08f2e30d2f7b397e6fb3410cbd3002a4baf2e4
WhoAmI <fabian.gregory98@hotmail.de>
2025-12-25 13:09:02 +0100

    Klasse Cell und Board erstellt und dann eine grundlogik fuer das Spiel erstellt.

---
+++ ConwaysGameOfLifeWorkshop/Board.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLifeWorkshop
+{
+    internal class Board
+    {
+        const char CHARACTER_ALIVE = '#';
+        const char CHARACTER_DEAD = '.';
+
+        public Cell[,] Fields { get; private set; }
+        public int Generation { get; private set; }
+
+        public Board(int width, int height)
+        {
+            Fields = new Cell[width, height];
+            Generation = 1;
+
+            for(int y = 0; y < Fields.GetLength(1); y++)
+            {
+                for(int x = 0; x < Fields.GetLength(0); x++)
+                {
+                    Fields[x, y] = new Cell(false);
+                }
+            }
+        }
+
+        public void Run()
+        {
+            Console.CursorVisible = false;
+
+            while(true)
+            {
+                //Draw();
+                //Update();
+                Generation++;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ ConwaysGameOfLifeWorkshop/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLifeWorkshop
+{
+    internal class Cell
+    {
+        public bool isAlive { get; set; }
+        public bool AliveNextRound { get; set; }
+        public bool LastGenerationStatus { get; set; }
+        public int AmountOfNeighbours { get; set; }
+        
+        public Cell(bool alive)
+        {
+            isAlive = alive;
+        }
+    }
+}
